df3550130fefae813f5f8b3663948c9df5563718
DimaTer89 <35215775+DimaTer89@users.noreply.github.com>
2018-03-04 13:49:48 +0300

    1.cpp

--- 1.cpp
+++ 1.cpp
@@ -16,3 +16,19 @@ void main() {
 	system("pause");
 }
 ============================
+#include <iostream>
+using namespace std;
+void main() {
+	setlocale(LC_ALL, "rus");
+	double weight;
+	double value;
+	printf(" Хотите узнать свой родиевый эквивалент ? \n");
+	printf(" Давайте подсчитаем \n");
+	printf(" Пожалуйста, введите свой вес, выраженный в фунтах: ");
+	cin>>weight;
+	value = 770.0 * weight * 14.5833;
+	printf(" Ваш родиевый эквивалент составляе %.2f \n", value);
+	printf(" Вы легко можете стать достойным этого ! Если цена родия падает,\n");
+	printf(" ешьте больше для поддержания своей стоимости.\n");
+	system("pause");
+}
